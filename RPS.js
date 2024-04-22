a1d33b6f7734610d7a791130545eb989950f6e04
Shubhham yadav <shubhamyadav960212@gmail.com>
2024-04-22 16:19:58 +0530

    Create RPS.js

---
+++ RPS.js
@@ -0,0 +1,58 @@
+let userScore=0;
+let compScore=0;
+const choices=document.querySelectorAll(".choice");
+const msg=document.querySelector("#msg");
+const userScorePara=document.querySelector("#user-score");
+const compScorePara=document.querySelector("#comp-score");
+choices.forEach((choice)=>{
+    choice.addEventListener("click",()=>{
+        const userChoice=choice.getAttribute("id");
+    // console.log("choice was clicked",userChoice);
+    playGame(userChoice);
+    });
+});
+const drawGame=()=>{
+    // console.log("game was draw");
+    msg.innerText="Game Was Draw! Play Again";
+    msg.style.backgroundColor="#081b31";
+};
+const showWinner=(userWin,userChoise,compChoice)=>{
+    if(userWin){
+        userScore++;
+        userScorePara.innerText=userScore;
+        // console.log("You Win!");
+       msg.innerText=`You Win! Your ${userChoise} beats ${compChoice}`;
+        msg.style.backgroundColor="green";
+    }else{
+        compScore++;
+        compScorePara.innerText=compScore;
+    // console.log("You lose!");
+    msg.innerText=`You Lose! Your ${compChoice} beats ${userChoise}`;
+    msg.style.backgroundColor="Red";
+    }
+};
+const playGame=(userChoice)=>{
+// console.log("user choice is ",userChoice);
+const compChoice=genComputerChoice();
+// console.log("computer choice is ",compChoice);
+
+if(userChoice===compChoice){
+    drawGame();
+}else{
+    let userWin=true;
+    if(userChoice==="rock"){
+        userWin=compChoice==="paper"?false:true;
+    }else if(userChoice==="paper"){
+        userWin=compChoice==="scissors"?false:true;
+    }else{
+        userWin=compChoice==="rock"?false:true;
+    }
+    showWinner(userWin ,userChoice,compChoice);
+}
+
+};
+const genComputerChoice=()=>{
+ const options=["rock","paper","scissors"];
+  const randIdx=Math.floor(Math.random()*3);
+  return options[randIdx];
+};
